b34de9bf3205923f2c23d2870cdbbdb95cc07ffa
andzej123 <andzejdemidov@gmail.com>
2024-01-20 19:12:47 +0200

    tasks

--- praktiniai_papildomai_1/papild_03.SaveableDictionary/src/main/java/dictionary/Main.java
+++ praktiniai_papildomai_1/papild_03.SaveableDictionary/src/main/java/dictionary/Main.java
@@ -2,32 +2,36 @@ package dictionary;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // You can test your dictionary here
 
-        SaveableDictionary dictionary = new SaveableDictionary("");
+        SaveableDictionary dictionary = new SaveableDictionary("C:\\Users\\Andzej\\Desktop\\java\\praktiniai_papildomai_1\\papild_03.SaveableDictionary\\words.txt");
         boolean wasSuccessful = dictionary.load();
         if (wasSuccessful) {
             System.out.println("Successfully loaded the dictionary from file");
         }
 
-        System.out.println(wasSuccessful);
+        dictionary.load();
         System.out.println(dictionary.translate("apina"));
         System.out.println(dictionary.translate("ohjelmointi"));
         System.out.println(dictionary.translate("alla oleva"));
+//        dictionary.printDic();
 //        dictionary.add("apina", "monkey");
-//        dictionary.add("banaani", "banana");
-//        dictionary.add("ohjelmonti", "programming");
+        dictionary.add("banaani", "banana");
+        dictionary.add("ohjelmonti", "programming");
+        dictionary.printDic();
 //        dictionary.delete("apina");
 //        dictionary.delete("banana");
-//
-//
-//        System.out.println(dictionary.translate("apina"));
-//        System.out.println(dictionary.translate("monkey"));
-//        System.out.println(dictionary.translate("banana"));
-//        System.out.println(dictionary.translate("banaani"));
-//        System.out.println(dictionary.translate("ohjelmonti"));
+        dictionary.save();
+
+
     }
 }
--- praktiniai_papildomai_1/papild_03.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ praktiniai_papildomai_1/papild_03.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -1,35 +1,71 @@
 package dictionary;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SaveableDictionary {
 
 
-    Map<String, String> dictionary = new HashMap<>();
+    private Map<String, String> dictionary;
 
-    File myFile;
+    private File myFile;
+
+    public SaveableDictionary() {
+        dictionary = new HashMap<>();
+    }
 
     public SaveableDictionary(String file) {
         myFile = new File(file);
+        dictionary = new HashMap<>();
     }
 
-    public boolean load() throws IOException {
-        if (myFile.canRead()) {
-            Files.lines(Paths.get(myFile.toURI()))
-                    .map(f -> f.split(":"))
-                    .map(f -> dictionary.put(f[0], f[1]));
-            return true;
-        } else {
-            return false;
+    public boolean load() {
+        try {
+            if (myFile.canRead()) {
+                Files.lines(Paths.get(myFile.toURI()))
+                        .map(f -> f.split(":"))
+                        .forEach(f -> dictionary.put(f[0], f[1]));
+//            Files.lines(Paths.get(myFile.toURI())).forEach(System.out::println);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+    }
+
+    public boolean save() {
+//        List<String> lines = Arrays.asList("The first line", "The second line");
+//        Path file = Paths.get("the-file-name.txt");
+//        Files.write(file, (Iterable<? extends CharSequence>) dictionary, StandardCharsets.UTF_8);
+//        for (Map.Entry<String,String> entry : dictionary.entrySet()) {
+//            properties.put(entry.getKey(), entry.getValue());
+//        }
+        try {
+            FileWriter myWriter = new FileWriter(myFile);
+            for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                myWriter.write(entry.getKey() + ":" + entry.getValue() + "\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+        return true;
+    }
+
+    public void printDic() {
+        System.out.println(dictionary);
     }
 
     public void add(String words, String translation) {
